55705d2c2e3c0796a4e85315906362315922ab6e
PLOheMfW <meg4mag@gmail.com>
2021-07-03 12:41:29 +0300

    add errorlog

--- python_parser_api/src/__init__.py
+++ python_parser_api/src/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_restful import Api
+from logging import FileHandler, WARNING
 
 app = Flask(__name__)
+
+api = Api(app)
+
+#logging customization
+file_error_handler = FileHandler('logs/errorlog.txt')
+file_error_handler.setLevel(WARNING)
+app.logger.addHandler(file_error_handler)
